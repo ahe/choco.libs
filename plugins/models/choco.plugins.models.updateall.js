4f4ffeb091943874d10317ff2c3412fc98fb5de4
Anthony Heukmes <anthony.heukmes@skynet.be>
2010-07-30 15:42:34 +0200

    Add js-model updateAll plugin

---
+++ plugins/models/choco.plugins.models.updateall.js
@@ -0,0 +1,35 @@
+// This js-model plugin brings the updateAll() method at the class level.
+// MyModel.updateAll() will serialize your collection and send a PUT request to your server.
+// {"records"=>{"1"=>{"title"=>"first post", "author"=>"antho"}, "2"=>{"title"=>"choco!", "author"=>"antho"}}}
+
+var UpdateAllPlugin = {
+	modelsClassMethods : {
+		updateAll: function(callback) {
+			var records  = this.all();
+			var name     = this._name.toLowerCase();
+			var params   = { records: {} };
+			params[name] = [];
+			
+			for(i=0; i < records.length; i++) {
+				var id = records[i].attr('id');
+				var attributes = records[i].attr();
+				delete attributes.id;
+				params['records'][id] = attributes;
+			}
+			
+			$.ajax({
+			  type: 'PUT',
+			  url: this.persistence.path() + '/all',
+			  data: params,
+			  complete: function(xhr, statusText) {
+					if(callback) {
+						callback(xhr, statusText);
+					}
+			  }
+			});
+		}
+	}
+};
+
+// Apply the js-model plugin
+ChocoUtils.modelPlugin(UpdateAllPlugin);
